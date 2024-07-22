3585ca9f7adc4ac00f90d8ec44096f6caccf1f59
tav gur <tav.gur.2008@gmail.com>
2024-07-22 13:11:44 +0200

    before barriers

--- Boids/boid.py
+++ Boids/boid.py
@@ -13,7 +13,7 @@ BOID_SIGHT_DISTANCE = 90
 BOID_PERSONAL_SPACE = 60
 BOID_MAX_SPEED = 120  # per second
 BOID_MAX_FORCE = 20
-BOID_ACCELERATION_FACTOR = 0.5
+BOID_ACCELERATION_FACTOR = 80
 BOID_MAX_VARIATION = math.radians(50)
 BOID_VARIATION_PERCENTAGE_PER_SECOND = 0.5
 
@@ -24,10 +24,10 @@ BOID_IMAGE = pygame.transform.flip(
     False)
 BOID_SIGHT_COLOR = (175, 255, 171)
 
-COHESION_FACTOR = 0.2
-SEPARATION_FACTOR = 500
-ALIGNMENT_FACTOR = 1
-
+COHESION_FACTOR = 0.5
+SEPARATION_FACTOR = 1.0
+ALIGNMENT_FACTOR = 1.0
+WALL_FACTOR = 0.0
 BARRIER_FACTOR = 10
 
 
@@ -76,14 +76,15 @@ class Boid:
         avg_y = sum([boid.y for boid in seen_boids]) / len(seen_boids)
         to_average_position = Vector(avg_x - self.x, avg_y - self.y)
         cohesion_force = to_average_position - self.direction
-        return cohesion_force
+        return cohesion_force * COHESION_FACTOR
 
     def alignment_force(self, seen_boids: list[Self]) -> Vector:
         if not seen_boids:
             return Vector(0, 0)
 
         average_direction = Vector.average([boid.direction for boid in seen_boids])
-        return average_direction - self.direction
+        alignment_force = average_direction - self.direction
+        return alignment_force * ALIGNMENT_FACTOR
 
     def separation_force(self, seen_boids: list[Self]) -> Vector:
         if not seen_boids:
@@ -98,10 +99,29 @@ class Boid:
         to_average_position = Vector(avg_x - self.x, avg_y - self.y)
         away_from_average = to_average_position.get_opposite()
         separation_force = away_from_average - self.direction
-        return separation_force
+        return separation_force * SEPARATION_FACTOR
 
-    def barrier_force(self, barriers) -> Vector:
+    def barrier_force(self, barriers):
         pass
+
+    def wall_force(self) -> Vector:
+
+        left_dist = self.x
+        right_dist = main_screen_width - self.x
+        top_dist = self.y
+        bottom_dist = main_screen_height - self.y
+
+        left_mult = max(BOID_SIGHT_DISTANCE - left_dist, 0)
+        right_mult = max(BOID_SIGHT_DISTANCE - right_dist, 0)
+        top_mult = max(BOID_SIGHT_DISTANCE - top_dist, 0)
+        bottom_mult = max(BOID_SIGHT_DISTANCE - bottom_dist, 0)
+
+        desired_direction = Vector(left_mult - right_mult, top_mult - bottom_mult)
+        desired_direction = Vector(right_mult - left_mult, bottom_mult - top_mult)
+        wall_force = desired_direction - self.direction
+        return wall_force * WALL_FACTOR
+
+
 
     def find_flock_direction(self, all_boids: list[Self], dt: float):
         seen_boids = [boid for boid in all_boids if self.distance_to(boid) < BOID_SIGHT_DISTANCE and boid != self]
@@ -113,10 +133,11 @@ class Boid:
         force += self.alignment_force(seen_boids)
         force += self.cohesion_force(seen_boids)
         force += self.separation_force(seen_boids)
+        force += self.wall_force()
         force.clamp_magnitude(BOID_MAX_FORCE)
 
         self.direction += force
-        self.direction *= 1 + BOID_ACCELERATION_FACTOR
+        self.direction *= BOID_ACCELERATION_FACTOR * dt
         self.direction.clamp_magnitude(BOID_MAX_SPEED)
 
         if random.random() < BOID_VARIATION_PERCENTAGE_PER_SECOND * dt:
